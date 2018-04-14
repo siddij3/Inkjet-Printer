25dbd52cfde15652d0202d82447b18489846851e
Junaid Siddiqui <junaidas97@gmail.com>
2018-04-14 16:32:08 -0400

    Prototyping
    CAD models, schematics and PIC Code

---
+++ PIC Code/PIC18F25K50.X/build/default/debug/pic18fmain.p1.d
@@ -0,0 +1,3 @@
+ build/default/debug/pic18fmain.d  \
+ build/default/debug/pic18fmain.p1:  \
+ pic18fmain.c 
---
+++ PIC Code/PIC18F25K50.X/build/default/production/pic18fmain.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/pic18fmain.d  \
+ build/default/production/pic18fmain.p1:  \
+ pic18fmain.c 
